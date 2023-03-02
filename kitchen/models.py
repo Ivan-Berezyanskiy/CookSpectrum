20b722b84af787e9cab6a2e773950d90cca5ce3f
Іван Березянський <mamanadarin@gmail.com>
2023-03-02 21:31:30 +0200

    Add Dish model to models.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class DishType(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
